60a349a3765afdb4c14fe3ca834f639c65723294
Mohamed ElAraby <mhelaraby@flynas.com>
2024-05-21 16:55:00 +0300

    End of section 9

---
+++ client-project/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import { Item, ItemContent, Button, SegmentGroup, Segment, ItemGroup, ItemImage, ItemHeader, ItemDescription, Icon } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+    activity: Activity
+}
+
+export default function ActivityListItem({ activity }: Props) {
+    return (
+        <SegmentGroup>
+            <Segment>
+                <ItemGroup>
+                    <Item>
+                        <ItemImage size='tiny' circular src='/assets/user.png' />
+                        <ItemContent>
+                            <ItemHeader as={Link} to={`/activities/${activity.id}`} > {activity.title}</ItemHeader>
+                            <ItemDescription>Hosted by Bob</ItemDescription>
+                        </ItemContent>
+                    </Item>
+                </ItemGroup>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {activity.date}
+                    <Icon name='marker' /> {activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendees goes here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button as={Link} to={`/activities/${activity.id}`} color='teal' floated='right' content='View' />
+            </Segment>
+        </SegmentGroup>
+    )
+}
